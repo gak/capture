dd4b96564452bdf3099827d61a3b15ca44460155
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2011-09-16 14:01:30 +1000

    using imagensap isntead of isightcapture

--- capture.sh
+++ capture.sh
@@ -1,16 +1,21 @@
-cd /Users/gak/src/capture/frames
+#!/bin/bash
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR/frames
 
 BASE=`date +%Y%m%d`
 D=$BASE
 D2=${BASE}_ip
 
 mkdir -p $D
-mkdir -p $D2 
+mkdir -p $D2
 
 BASE=`date +%s`
 F=$BASE.jpg
 F2=$BASE.ip
 
-../isightcapture $D/$F > /dev/null 2>&1 
+killall -9 imagesnap > /dev/null 2>&1
+
+../imagesnap $D/$F > /dev/null 2>&1
 /sbin/ifconfig | grep --color=none "inet " | grep -v --color=none 127.0.0.1 > $D2/$F2
 
