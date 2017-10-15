dd3e7b3f291893524a1ac8dbf3948e85702531e0
Gerald Kaszuba <gak@gak0.com>
2017-10-16 09:06:36 +1100

    fixed isight to work in new macos

--- capture.sh
+++ capture.sh
@@ -19,7 +19,9 @@ F2=$BASE.ip
 #fswebcam --no-banner --jpeg 95 --no-overlay -d /dev/video0 -r 1280x720 -S 20 $D/$Fa
 #fswebcam --no-banner --jpeg 95 --no-overlay -d /dev/video1 -r 1280x720 -S 20 $D/$Fb
 
-/usr/local/bin/imagesnap -v -w 3 $D/$Fa
+# /usr/local/bin/imagesnap -v -w 1 $D/$Fa
+
+ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i "0" -vframes 1 $D/$Fa
 
 /sbin/ifconfig | grep --color=none "inet " | grep -v --color=none 127.0.0.1 > $D2/$F2
 
